e121d281eadf907e0a3b94d71276f47bda008236
Mirbek <78426144+MamashovMirbek@users.noreply.github.com>
2024-05-15 12:56:44 +0600

    FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,23 +1,23 @@
-import webbrowser # Importing for Google searching
-import speech_recognition as sr # Importing for speech recognition
+import webbrowser
+import speech_recognition as sr
 
-recognizer = sr.Recognizer() # Initializing the recognizer
+recognizer = sr.Recognizer()
 
-with sr.Microphone() as source: # Using the microphone as the audio source
-    print('Listening...') # Hint for the user for speaking
+with sr.Microphone() as source:
+    print('Listening...')
 
-    audio = recognizer.listen(source) # Listening for speech and converting it to text
+    audio = recognizer.listen(source)
 
 try: # tries the lines below
-    your_speech = recognizer.recognize_google(audio) # Recognizing speech using Google Speech Recognition
-    print('You said: "' + your_speech + '"') # Printing the speech
+    your_speech = recognizer.recognize_google(audio)
+    print('You said: "' + your_speech + '"')
 
-    if your_speech != '': # Means if your speech is not empty or has a result
-        url = 'https://www.google.com/search?q=' + '+'.join(your_speech.split()) # Adding 'https...' and changing spaces to '+' for web browsing url
-        webbrowser.open_new(url) # Opening the web browser and searching for your speech
+    if your_speech != '':
+        url = 'https://www.google.com/search?q=' + '+'.join(your_speech.split())
+        webbrowser.open_new(url)
 
-except sr.UnknownValueError: # Means if 'try:' is not defined then this line will work
-    print("Sorry, I didn't catch that.") # Means if line 19 is true then it prints
+except sr.UnknownValueError:
+    print("Sorry, I didn't catch that.")
 
-except sr.RequestError: # Means if line 19 didn't work then this line will work
-    print("Could not request results. Please check your internet connection.") # Means if line 22 is true then it prints
+except sr.RequestError:
+    print("Could not request results. Please check your internet connection.")
